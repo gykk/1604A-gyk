b3d3b02975a9fab463dd05f2d4f0c160d9d04c1e
Administrator <1074146476@qq.com>
2018-11-06 18:56:55 +0800

    111

---
+++ 2018-11-6.php
@@ -0,0 +1,12 @@
+ function nu($n,$m) {
+    $number = 0;
+    for($i=$n;$i<=$m;$i++) {
+        $a = substr_count((string)$i,"1");
+        if($a!==false) {
+            $number = $number+$a;
+        }
+        //echo $i."\n";
+    }
+    echo $number;
+}
+nu(1,13);

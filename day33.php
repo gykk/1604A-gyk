74260162dc2f471301fd10a50d4ae7a0027cca5d
Administrator <1074146476@qq.com>
2018-11-14 08:34:51 +0800

    44545

---
+++ day33.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/14 0014
+ * Time: 8:14
+ */
+//
+//public int Add(int num1,int num2) {
+// while(num2!=0){
+//        int sum = num1^num2;
+//  num2 = (num1&num2)<<1;
+// num1 = sum;
+//}
+//return num1;
+//}
+$a = 10;
+$b = 20;
+$c = $a + $b;
+echo $c;
